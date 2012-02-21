944df84e1c53e945fd192598976479532965fcad
Owen Davies <owendavies1993@hotmail.com>
2012-02-21 00:22:15 +0000

    Stage 1 done.

--- src/spreadsheet/Main.java
+++ src/spreadsheet/Main.java
@@ -1,5 +1,7 @@
 package spreadsheet;
 
+import spreadsheet.api.SpreadsheetInterface;
+import spreadsheet.gui.SpreadsheetGUI;
 
 public class Main {
 
@@ -7,6 +9,17 @@ public class Main {
     private static final int DEFAULT_NUM_COLUMNS = 5000;
 
     public static void main(String[] args) {
-    }
+        SpreadsheetInterface s = new Spreadsheet();
+        SpreadsheetGUI gui;
 
+        if (args.length == 2) {
+            gui =
+                    new SpreadsheetGUI(s, Integer.parseInt(args[0]),
+                            Integer.parseInt(args[1]));
+        } else {
+            gui = new SpreadsheetGUI(s, DEFAULT_NUM_ROWS, DEFAULT_NUM_COLUMNS);
+        }
+
+        gui.start();
+    }
 }
--- src/spreadsheet/Spreadsheet.java
+++ src/spreadsheet/Spreadsheet.java
@@ -1,6 +1,31 @@
 package spreadsheet;
 
+import spreadsheet.api.CellLocation;
 import spreadsheet.api.SpreadsheetInterface;
+import spreadsheet.api.value.Value;
+import spreadsheet.api.value.*;
 
 public class Spreadsheet implements SpreadsheetInterface {
+
+    @Override
+    public void setExpression(CellLocation location, String expression) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getExpression(CellLocation location) {
+        return "";
+    }
+
+    @Override
+    public Value getValue(CellLocation location) {
+        return new InvalidValue("");
+    }
+
+    @Override
+    public void recompute() {
+        // TODO Auto-generated method stub
+
+    }
 }
