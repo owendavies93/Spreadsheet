2e44b95ff130032ad5ac1ab6236022d297ebb327
Owen Davies <owendavies1993@hotmail.com>
2012-02-24 23:05:07 +0000

    Stage 3 is working...I think.

--- src/spreadsheet/Cell.java
+++ src/spreadsheet/Cell.java
@@ -1,21 +1,33 @@
 package spreadsheet;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import spreadsheet.api.CellLocation;
+import spreadsheet.api.ExpressionUtils;
+import spreadsheet.api.observer.Observer;
 import spreadsheet.api.value.InvalidValue;
 import spreadsheet.api.value.Value;
 
-public class Cell {
+public class Cell implements Observer<Cell> {
 
-    private Spreadsheet sheet;
-    private CellLocation loc;
+    private final Spreadsheet sheet;
+    private final CellLocation loc;
+
     private Value val;
     private String expr;
 
-    public Cell(Spreadsheet sheet, CellLocation loc, String expr) {
+    // What do I depend on?
+    private Set<Cell> thisReferences = new HashSet<Cell>();
+
+    // What depends on me?
+    private Set<Observer<Cell>> referencesMe = new HashSet<Observer<Cell>>();
+
+    public Cell(Spreadsheet sheet, CellLocation loc) {
         this.sheet = sheet;
         this.loc = loc;
-        this.setVal(new InvalidValue(""));
-        this.setExpr(expr);
+        this.setVal(null);
+        this.setExpr("");
     }
 
     public Value getVal() {
@@ -30,8 +42,54 @@ public class Cell {
         return expr;
     }
 
-    public void setExpr(String expr) {
-        this.expr = expr;
+    public void setExpr(String newExpr) {
+        for (Cell c : thisReferences) {
+            c.referencesMe.remove(this);
+        }
+        thisReferences.clear();
+
+        this.expr = newExpr;
+        setVal(new InvalidValue(newExpr));
+        addToInvalid();
+
+        Set<CellLocation> locs =
+                ExpressionUtils.getReferencedLocations(newExpr);
+
+        for (CellLocation l : locs) {
+            sheet.setExpression(l, sheet.getExpression(l)); // perhaps this is
+                                                            // wrong
+            Cell c = sheet.getCellAt(l);
+            thisReferences.add(c);
+            c.referencesMe.add(this);
+        }
+
+        for (Observer<Cell> c : referencesMe) {
+            c.update(this);
+        }
+    }
+
+    public boolean isInInvalidSet() {
+        return sheet.getInvalid().contains(this);
+    }
+
+    public void addToInvalid() {
+        sheet.getInvalid().add(this);
+    }
+
+    @Override
+    public void update(Cell changed) {
+        if (!isInInvalidSet()) {
+            changed.addToInvalid();
+            changed.setVal(new InvalidValue(changed.getExpr()));
+
+            for (Observer<Cell> obs : referencesMe) {
+                obs.update(changed);
+            }
+        }
+    }
+
+    public String toString() {
+        return "(" + loc + " -> " + val.toString() + ")";
     }
 
 }
--- src/spreadsheet/Spreadsheet.java
+++ src/spreadsheet/Spreadsheet.java
@@ -1,7 +1,11 @@
 package spreadsheet;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+
 import spreadsheet.api.CellLocation;
 import spreadsheet.api.SpreadsheetInterface;
 import spreadsheet.api.value.*;
@@ -11,34 +15,50 @@ public class Spreadsheet implements SpreadsheetInterface {
     private Map<CellLocation, Cell> locations =
             new HashMap<CellLocation, Cell>();
 
+    private Set<Cell> invalid = new HashSet<Cell>();
+
     @Override
     public void setExpression(CellLocation location, String expr) {
-        if (locations.get(location) != null) {
-            Cell c = locations.get(location);
+        if (getCellAt(location) != null) {
+            Cell c = getCellAt(location);
             c.setExpr(expr);
-            c.setVal(new StringValue(expr));
+            c.setVal(new InvalidValue(expr)); // You changed this
         } else {
-            Cell c = new Cell(this, location, expr);
-            c.setVal(new StringValue(expr));
+            Cell c = new Cell(this, location);
+            c.setExpr(expr);
+            c.setVal(new InvalidValue(expr)); // and this
             locations.put(location, c);
         }
     }
 
     @Override
     public String getExpression(CellLocation location) {
-        Cell c = locations.get(location);
+        Cell c = getCellAt(location);
         return c != null ? c.getExpr() : "";
     }
 
     @Override
     public Value getValue(CellLocation location) {
-        Cell c = locations.get(location);
-        return c != null ? c.getVal() : new InvalidValue("");
+        Cell c = getCellAt(location);
+        return c != null ? c.getVal() : null;
     }
 
     @Override
     public void recompute() {
-        // TODO Auto-generated method stub
+        Iterator<Cell> i = invalid.iterator();
+        while (i.hasNext()) {
+            Cell c = i.next();
+            System.out.println(c);
+            c.setVal(new StringValue(c.getExpr()));
+            i.remove();
+        }
+    }
 
+    public Set<Cell> getInvalid() {
+        return invalid;
+    }
+
+    public Cell getCellAt(CellLocation loc) {
+        return locations.get(loc);
     }
 }
